159c81d0f3e8d32d988c011ae9d92a5735f17177
renatomoor <renatomoor1@gmail.com>
2022-08-27 17:31:18 +0000

    Deploying to gh-pages from  @ 838950c9ee08b25def2f059ada7967c56d7df8bd 🚀

---
+++ assets/config.e6780329.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.4e35c821.js";import"./iframe.fd30d5f8.js";import"./vue.esm-bundler.12ec22f2.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.e6780329.js.map
