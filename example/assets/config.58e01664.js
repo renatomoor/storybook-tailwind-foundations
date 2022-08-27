584953896b66423341d3a53f7385bb0f82d9cf60
renatomoor <renatomoor@users.noreply.github.com>
2022-08-27 21:52:39 +0000

    deploy: e0b1c73c2586f326d9e304bb8ec588924893b1ea

--- example/assets/config.58e01664.js
+++
@@ -1,2 +0,0 @@
-import{d as t,r as m,a as s}from"./decorateStory.15d104ff.js";import"./iframe.79dbd0f5.js";import"./vue.esm-bundler.12ec22f2.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
-//# sourceMappingURL=config.58e01664.js.map
