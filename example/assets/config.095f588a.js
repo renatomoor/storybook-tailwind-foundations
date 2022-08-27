0e92c82dda3bcfddcec7fb4a15994d6089f51f76
renatomoor <renatomoor@users.noreply.github.com>
2022-08-27 22:46:13 +0000

    deploy: 25c1142e646cf780e6b14df5224441b82511e197

---
+++ example/assets/config.095f588a.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.452783e1.js";import"./iframe.dccf68f0.js";import"./vue.esm-bundler.12ec22f2.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.095f588a.js.map
--- example/assets/es.map.constructor.a353256b.js
+++
@@ -1,2 +0,0 @@
-import{h as t,i as o}from"./iframe.d589e8ae.js";var r=t,e=o;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
-//# sourceMappingURL=es.map.constructor.a353256b.js.map
